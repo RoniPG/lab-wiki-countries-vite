9f44cdccec690dcaaca368345d7689671c1b4257
RoniPG <ronigmn@gmail.com>
2024-12-03 16:57:49 +0100

    Iteration 1 | Create Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      {/* <HomePage /> */}
+      {/* <CountryDetails /> */}
     </div>
   );
 }
