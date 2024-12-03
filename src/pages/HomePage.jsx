cddb2bf7a4f593e01939702f47e30407c2abcadb
RoniPG <ronigmn@gmail.com>
2024-12-03 18:25:48 +0100

    Iteration 3 | Fetch Countries Data and Display as a List

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://ih-countries-api.herokuapp.com";
+
 function HomePage() {
+    const [countries, setCountries] = useState([]);
+
+    const getCountries = () => {
+        axios
+            .get(`${BASE_URL}/countries`)
+            .then((response) => {
+                const countriesData = response.data;
+                setCountries(countriesData)
+            })
+            .catch(err => console.log(err));
+    };
+    useEffect(() => {
+        getCountries();
+    }, [])
     return (
-        <div className="container" style={{maxHeight: "90vh", overflow : "scroll"}}>
-            <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
+        <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+            <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
+            <div className="list-group">
+                {countries.map(country => (
+                    <a key={country._id} className="list-group-item list-group-item-action">
+                        {country.name.official}
+                    </a>
+                ))}
+            </div>
         </div>
     );
 }
