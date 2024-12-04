991ceeeae7a2ebbdd24fa9092901f93f830054ba
RoniPG <ronigmn@gmail.com>
2024-12-04 01:19:09 +0100

    Iteration 4 | Country List with Links

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,7 +1,31 @@
 function CountryDetails() {
     return (
         <div className="container">
-            <p style={{fontSize: "24px", fontWeight: "bold"}}>Country Details</p>
+            <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+            <img src="" alt="flag-of-country" />
+            <h1>COUNTRY NAME</h1>
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }}>Capital</td>
+                        <td>CAPITAL</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>AREA <sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                <li><a href="#">COUNTRY 1</a></li>
+                                <li><a href="#">COUNTRY 2</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const BASE_URL = "https://ih-countries-api.herokuapp.com";
 
@@ -22,10 +23,17 @@ function HomePage() {
         <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
             <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
             <div className="list-group">
+                {console.log(countries)}
+                
                 {countries.map(country => (
-                    <a key={country._id} className="list-group-item list-group-item-action">
-                        {country.name.official}
-                    </a>
+                    <Link
+                        key={country._id}
+                        className="list-group-item list-group-item-action"
+                        to={`/${country.alpha3Code}`}
+                    >
+                        <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.official} />
+                        <p>{country.name.official}</p>
+                    </Link>
                 ))}
             </div>
         </div>
