cb147e972901004d9118a7db50cf07deae679fa3
RoniPG <ronigmn@gmail.com>
2024-12-04 02:00:14 +0100

    5.3| Display Loading Message While Fetching Data

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 const BASE_URL = "https://ih-countries-api.herokuapp.com";
 
 function CountryDetails() {
-    const [country, setCountry] = useState([]);
+    const [country, setCountry] = useState(null);
     const { countryId } = useParams();
 
     useEffect(() => {
@@ -20,30 +20,34 @@ function CountryDetails() {
     return (
         <div className="container">
             <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
-            <img src="" alt="flag-of-country" />
-            <h1>COUNTRY NAME</h1>
-            <table className="table">
-                <thead></thead>
-                <tbody>
-                    <tr>
-                        <td style={{ width: "30%" }}>Capital</td>
-                        <td>CAPITAL</td>
-                    </tr>
-                    <tr>
-                        <td>Area</td>
-                        <td>AREA <sup>2</sup></td>
-                    </tr>
-                    <tr>
-                        <td>Borders</td>
-                        <td>
-                            <ul>
-                                <li><a href="#">COUNTRY 1</a></li>
-                                <li><a href="#">COUNTRY 2</a></li>
-                            </ul>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            {country === undefined || country === null
+                ? <p>Loading...</p>
+                : <>
+                    <img src="" alt="flag-of-country" />
+                    <h1>COUNTRY NAME</h1>
+                    <table className="table">
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <td style={{ width: "30%" }}>Capital</td>
+                                <td>CAPITAL</td>
+                            </tr>
+                            <tr>
+                                <td>Area</td>
+                                <td>AREA <sup>2</sup></td>
+                            </tr>
+                            <tr>
+                                <td>Borders</td>
+                                <td>
+                                    <ul>
+                                        <li><a href="#">COUNTRY 1</a></li>
+                                        <li><a href="#">COUNTRY 2</a></li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>}
         </div>
     );
 }
